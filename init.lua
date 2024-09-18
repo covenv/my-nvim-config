39dc1af8214898c2bdbc9aa68a66ee66576ee611
amber <mtganea@gmail.com>
2024-09-17 20:37:33 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require("config.lazy")
 vim.cmd.colorscheme "retrobox"
+vim.api.nvim_set_keymap('n', 'dd', '\"_dd', { noremap = true, silent = true })
 vim.opt.wrap = false
 vim.opt.number = true
 vim.g.loaded_matchparen = true
